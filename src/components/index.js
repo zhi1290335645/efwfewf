b1cc9527615ef2f0e99d20f5bb605077eb4a6902
zhi1290335645 <793293398@qq.com>
2019-12-22 16:08:02 +0800

    注册面包屑

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import layoutAside from './home/layout-aside'
 import layoutHeader from './home/layout-header'
-
+// import BreadCrumb from './common/bread-crumb'
+import BreadCrumb from './common/bread-crumb'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside) // 全局注册
     Vue.component('layout-header', layoutHeader)// 全局注册
+    Vue.component('bread-crumb', BreadCrumb)// 全局注册
   }
 }
