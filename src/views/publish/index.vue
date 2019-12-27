6fc07424dcfb52c8a1d1499696e3dc30c1b9dde8
zhi1290335645 <793293398@qq.com>
2019-12-27 16:25:04 +0800

    内容频道数据加载

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -20,12 +20,13 @@
           </el-radio-group>
         </el-form-item>
         <el-form-item label="频道">
-          <el-select></el-select>
+          <el-select>
+            <el-option v-for="item in channels" :key="item.id" :value="item.id" :label="item.name"></el-option>
+          </el-select>
         </el-form-item>
         <el-form-item>
           <el-button type='primary'>发布</el-button>
           <el-button>存入草稿</el-button>
-
         </el-form-item>
       </el-form>
   </el-card>
@@ -33,7 +34,26 @@
 
 <script>
 export default {
+  data () {
+    return {
+      channels: [] // 定义一个channels 接收频道
+    }
+  },
+  methods: {
+    //   获取频道
+    getChannels () {
+      this.$axios({
+        url: '/channels'
+      }).then(result => {
+        this.channels = result.data.channels // 获取频道数据
+      })
+    }
+  },
+  created () {
+    this.getChannels() // 获取频道数据
+  }
 }
 </script>
-<style  lang='less' scoped>
+
+<style>
 </style>
