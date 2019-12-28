2f46b02b2a2ede03a3094e6733be4288be79b25e
zhi1290335645 <793293398@qq.com>
2019-12-28 20:29:53 +0800

    404页面应用

---
+++ src/views/404.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class='not-found'>
+       <img src="../assets/404.png" alt="">
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang='less' scoped>
+  .not-found {
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items:center;
+  }
+</style>
