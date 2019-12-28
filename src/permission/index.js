92977768f8614054cad4aa6cac3427edc00b3068
zhi1290335645 <793293398@qq.com>
2019-12-28 20:00:56 +0800

    页面进度条引入应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,9 +1,13 @@
 // 权限拦截 导航守卫
 import router from '../router'
+import progress from 'nprogress'
+import 'nprogress/nprogress.css'
 // 全局守卫
 router.beforeEach(function (to, from, next) {
+  progress.start() // 开启进度条
+  // 拦截谁判断地址就写谁
+  // 经行权限判断 判断有无token 有的话 放过 没有回到登录页面
   if (to.path.startsWith('/home')) {
-    // 经行权限判断 判断有无token 有的话 放过 没有回到登录页面
     let token = window.localStorage.getItem('user-token')
     if (token) {
       next()// 直接放过
@@ -14,4 +18,10 @@ router.beforeEach(function (to, from, next) {
     next()// 直接放行
   }
   // next() 直接放行
+})
+// afterEach 关闭
+
+router.afterEach(function () {
+  progress.done()
+  // setTimeout(() => progress.done(), 500) // 关闭进度条
 })
